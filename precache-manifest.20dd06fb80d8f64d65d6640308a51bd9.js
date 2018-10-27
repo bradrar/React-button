3bb57587322234038d4a4e9ed99cb385d374350d
Brad Baguyo <bradbuzy@gmail.com>
2018-10-27 20:34:43 +0800

    Updates

---
+++ precache-manifest.20dd06fb80d8f64d65d6640308a51bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-button/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97c53af12174cb895e55",
+    "url": "/React-button/static/js/runtime~main.97c53af1.js"
+  },
+  {
+    "revision": "ca405258ee3cc032c5b7",
+    "url": "/React-button/static/js/main.ca405258.chunk.js"
+  },
+  {
+    "revision": "4d34d0425c0e81d464a2",
+    "url": "/React-button/static/js/1.4d34d042.chunk.js"
+  },
+  {
+    "revision": "ca405258ee3cc032c5b7",
+    "url": "/React-button/static/css/main.2699c079.chunk.css"
+  },
+  {
+    "revision": "569ae6bcce77b3b53999dcddc95cd1c6",
+    "url": "/React-button/index.html"
+  }
+];
