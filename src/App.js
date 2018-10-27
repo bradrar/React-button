318c8376b6ea2c7e2bed2f6aece2ced907d09ba9
Brad Baguyo <bradbuzy@gmail.com>
2018-10-27 20:22:39 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,74 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+/*
+Challenge Goal:
+
+Add functionality to the buttons on the list so that when UP is clicked the list item swaps with the item directly on top of it.
+
+When DOWN is pressed, the list item should swap with the item directly bellow it.
+*/
+
+function buttonUp(arr, fromIndex, toIndex) {
+  var element = arr[fromIndex];
+  arr.splice(fromIndex, 1);
+  arr.splice(toIndex, 0, element);
+  return arr;
+}
+
+class App extends React.Component {
+	constructor(props){
+  	super(props);
+    
+    this._handleItemSort = this._handleItemSort.bind(this);
+        
+  	this.state = {
+    	shoppingList: ['Bananas', 'Apples', 'Rice', 'Eggs' , 'GTX 1080Ti', 'Avocado']
+    }
+  }
+  
+  
+  
+  _handleItemSort(dir, currentIndex) {
+  	// create new list of items from a spread of our current shoppingList state.
+    // we don't want to reference the actual state and mutate it! 😱
+    const shoppingList = [...this.state.shoppingList]
+
+    if (dir === "up" ){
+      this.setState({
+        shoppingList: buttonUp(shoppingList, currentIndex , currentIndex - 1) 
+      }) 
+    } else {
+      this.setState({
+        shoppingList: buttonUp(shoppingList, currentIndex , currentIndex + 1) 
+      }) 
+    }
+   	
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return( 
+      <div>
+        <h1>UNUM Challenge</h1>
+        <ol>
+          {this.state.shoppingList.map((item, index) =>
+            	(<li data-index = {index} key={index}>
+                {item}
+                <div className='controls'>
+                  <button 
+                    disabled={index === 0} 
+                    onClick={ () => this._handleItemSort("up", index) }>UP</button>
+                  <button
+                    disabled={this.state.shoppingList.length === index + 1}
+                    onClick={ () => this._handleItemSort("down", index) } >DOWN</button>
+                </div>
+              </li>)
+            )}
+        </ol>
       </div>
     );
   }
 }
+
 
 export default App;
